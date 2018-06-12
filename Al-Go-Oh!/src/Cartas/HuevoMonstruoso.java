aa879adf9675839bb5d5250dd003949ed48edf31
FranR96 <fran7ruiz9@gmail.com>
2018-06-12 13:08:28 -0300

    Aca están unos ejemplos de carta Monstruo

---
+++ Al-Go-Oh!/src/Cartas/HuevoMonstruoso.java
@@ -0,0 +1,15 @@
+package Cartas;
+
+public class HuevoMonstruoso extends CartaMonstruo{
+
+	public HuevoMonstruoso() {
+		
+		this.ptsAtaque=600;
+		this.ptsDefensa=900;
+		this.estrellas=3;
+		
+	}
+	
+	public void efecto() {}
+	
+}
