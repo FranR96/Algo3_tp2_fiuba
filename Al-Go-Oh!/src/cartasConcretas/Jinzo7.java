0e71ef27d3ebd10bbb52d62342415a289305ca42
MatiasPriano <40174479+MatiasPriano@users.noreply.github.com>
2018-06-21 00:45:08 -0300

    aplicarEfecto agregado

--- Al-Go-Oh!/src/cartasConcretas/Jinzo7.java
+++ Al-Go-Oh!/src/cartasConcretas/Jinzo7.java
@@ -8,4 +8,10 @@ public class Jinzo7 extends CartaMonstruo{
 	public Jinzo7() {
 		super(500,400,2, new EfectoJinzo7(500));
 	}
+	
+	@Override
+	public void aplicarEfecto(Campo campo, Campo campoEnemigo, Jugador activo, Jugador oponente) {
+		
+		this.efecto.aplicarEfectoSobreJugadorOponente(oponente);
+	}
 }
