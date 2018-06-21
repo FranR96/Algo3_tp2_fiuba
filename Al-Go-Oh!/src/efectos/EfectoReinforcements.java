65afa25b02b65f0831f6a9a592ec684c51d8f085
MatiasPriano <40174479+MatiasPriano@users.noreply.github.com>
2018-06-21 09:17:59 -0300

    Add files via upload

---
+++ Al-Go-Oh!/src/efectos/EfectoReinforcements.java
@@ -0,0 +1,14 @@
+package efectos;
+
+import alGoOh.Campo;
+import cartas.CartaMonstruo;
+
+public class EfectoReinforcements extends Efecto {
+
+	@Override
+	public void aplicarEfectoSobreCampoActivo(Campo campo){
+		
+//		CartaMonstruo monstruo = campo.ultimoMonstruoAtacado();
+//		monstruo.setPtsAtaque(monstruo.getPtsAtaque() + 500);
+    }
+}
