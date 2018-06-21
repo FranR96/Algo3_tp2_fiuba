7912927b35a158784966f10c37acb035e752929d
MatiasPriano <40174479+MatiasPriano@users.noreply.github.com>
2018-06-21 09:17:30 -0300

    Add files via upload

---
+++ Al-Go-Oh!/src/cartasConcretas/Reinforcements.java
@@ -0,0 +1,22 @@
+package cartasConcretas;
+
+import alGoOh.Campo;
+import alGoOh.Jugador;
+import cartas.CartaTrampa;
+import efectos.EfectoReinforcements;
+
+public class Reinforcements extends CartaTrampa {
+	
+	public Reinforcements() {
+		
+		this.efecto = new EfectoReinforcements();
+	}
+	
+	@Override
+	public void aplicarEfecto(Campo campo, Campo campoEnemigo, Jugador activo, Jugador oponente) {
+		
+		this.efecto.aplicarEfectoSobreCampoActivo(campoEnemigo);
+		this.campo.eliminarCartaEspecial(this);
+	}
+
+}
