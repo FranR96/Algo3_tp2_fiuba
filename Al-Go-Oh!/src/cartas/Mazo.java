9470f00c92facbb2377b7c5cf4f32b635ccbf2d1
MauRodriguez <40152200+MauRodriguez@users.noreply.github.com>
2018-06-23 13:48:32 -0300

    Creación del mazo

--- Al-Go-Oh!/src/cartas/Mazo.java
+++ Al-Go-Oh!/src/cartas/Mazo.java
@@ -1,32 +1,77 @@
 package cartas;
 
+import cartasConcretas.*;
+
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Stack;
+import java.util.Collections;
 
 public class Mazo {
 
-	private Stack<Carta> mazo = new Stack<Carta>();
+	private ArrayList<Carta> mazo = new ArrayList<Carta>();
 	
 	public Mazo() {
 		this.inicializarMazo(mazo);
-		
 	}
 	
 	public void inicializarMazo(Collection<Carta> mazo) {
-		for(int i = 0; i<40; i++) {
-			//Carta  carta = Seleccionar Random de una coleccion de todas cartas.
-		//	mazo.push(carta);
-		}
-		
+		//Obligatorias (11)
+		this.mazo.add(new Wasteland());
+		this.mazo.add(new Sogen());
+		this.mazo.add(new BrazoDerechoExodia());
+		this.mazo.add(new BrazoIzquierdoExodia());
+		this.mazo.add(new PiernaDerechaExodia());
+		this.mazo.add(new PiernaIzquierdaExodia());
+		this.mazo.add(new Exodia());
+		this.mazo.add(new DragonBlancoDeOjosAzules());
+		this.mazo.add(new DragonBlancoDeOjosAzules());
+		this.mazo.add(new DragonBlancoDeOjosAzules());
+		this.mazo.add(new DragonDefinitivoDeOjosAzules());
+
+		//Magicas (8)
+		this.mazo.add(new AgujeroNegro());
+		this.mazo.add(new AgujeroNegro());
+		this.mazo.add(new Fisura());
+		this.mazo.add(new Fisura());
+		this.mazo.add(new DianKetoLaCurandera());
+		this.mazo.add(new DianKetoLaCurandera());
+		this.mazo.add(new OllaDeLaCodicia());
+		this.mazo.add(new OllaDeLaCodicia());
+
+		//Trampa (6)
+		this.mazo.add(new CilindroMagico());
+		this.mazo.add(new CilindroMagico());
+		this.mazo.add(new JustDesserts());
+		this.mazo.add(new JustDesserts());
+		this.mazo.add(new Reinforcements());
+		this.mazo.add(new Reinforcements());
+
+		//Mostro (15)
+		this.mazo.add(new HuevoMonstruoso());
+		this.mazo.add(new InsectoComeHombres());
+		this.mazo.add(new Jinzo7());
+		this.mazo.add(new AbismoReluciente());
+		this.mazo.add(new AbismoReluciente());
+		this.mazo.add(new Ansatsu());
+		this.mazo.add(new Ansatsu());
+		this.mazo.add(new DragonDeAlexandrita());
+		this.mazo.add(new HombreFrijolJerry());
+		this.mazo.add(new KappaPsiquico());
+		this.mazo.add(new KappaPsiquico());
+		this.mazo.add(new LoboGigaTecnologico());
+		this.mazo.add(new MagoOscuro());
+		this.mazo.add(new MaldicionDeDragon());
+		this.mazo.add(new PetitMoth());
+
+		//Se mezcla el mazo.
+		Collections.shuffle(this.mazo);
 	}
 	
 	public Carta tomarCartaDelMazo() {
-		return mazo.pop();
+		return mazo.remove(0);
 	}
 
 	public boolean estaVacio() {
-		return mazo.empty();
-	}
-	
-	
+		return mazo.isEmpty();
+	}	
 }
