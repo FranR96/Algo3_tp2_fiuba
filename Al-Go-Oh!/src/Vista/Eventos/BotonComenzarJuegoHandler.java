807947b4212e5cd6aee8c1ac0d810c9205b11d2b
komod0 <juliancrespo15@gmail.com>
2018-06-24 16:56:15 -0300

    Corregidos paths de las imagenes

--- Al-Go-Oh!/src/Vista/Eventos/BotonComenzarJuegoHandler.java
+++ Al-Go-Oh!/src/Vista/Eventos/BotonComenzarJuegoHandler.java
@@ -1,9 +1,13 @@
 package Vista.Eventos;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class BotonComenzarJuegoHandler implements EventHandler<ActionEvent> {
 
@@ -16,6 +20,10 @@ public class BotonComenzarJuegoHandler implements EventHandler<ActionEvent> {
     }
     @Override
     public void handle(ActionEvent event) {
-        stage.setScene(proximaEscena);
+        Timeline timeline = new Timeline();
+        KeyFrame key = new KeyFrame(Duration.millis(1500), new KeyValue(stage.getScene().getRoot().opacityProperty(), 0));
+        timeline.getKeyFrames().add(key);
+        timeline.setOnFinished((ae) -> stage.setScene(proximaEscena));
+        timeline.play();
     }
 }
