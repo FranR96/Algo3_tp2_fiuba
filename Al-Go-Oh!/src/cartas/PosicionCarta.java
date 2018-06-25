1936d3ae3e85a1e229b252097e085038e03137b4
MatiasPriano <40174479+MatiasPriano@users.noreply.github.com>
2018-06-25 18:07:48 -0300

    cambiarPosicion()

--- Al-Go-Oh!/src/cartas/PosicionCarta.java
+++ Al-Go-Oh!/src/cartas/PosicionCarta.java
@@ -5,4 +5,5 @@ public interface PosicionCarta {
 	void setCarta(CartaMonstruo monstruo);
 	int recibirDanio(int danio);
 	void atacar(CartaMonstruo monstruo1, CartaMonstruo monstruo2);
+	PosicionCarta cambiarPosicion();
 }
