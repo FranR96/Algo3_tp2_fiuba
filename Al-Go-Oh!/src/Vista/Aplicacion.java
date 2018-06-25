5dc70000226aff30e15950f536f16d97dad14487
komod0 <juliancrespo15@gmail.com>
2018-06-25 00:11:03 -0300

    Agregada pantalla de pedir nombre jugadoresy toda la logica asociada

--- Al-Go-Oh!/src/Vista/Aplicacion.java
+++ Al-Go-Oh!/src/Vista/Aplicacion.java
@@ -7,7 +7,6 @@ import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 
-
 public class Aplicacion extends Application
 {
     public static void main(String[] args)
@@ -19,11 +18,18 @@ public class Aplicacion extends Application
     {
         stage.setTitle("Al-Go-Oh!");
 
-        TableroDeJuego tableroDeJuego = new TableroDeJuego(stage);
-        Scene escenaTablero = new Scene(tableroDeJuego, 640, 480);
+        InfoContainer informacion = new InfoContainer();
 
-        VentanaInicio ventanaInicio = new VentanaInicio(stage, escenaTablero);
-        Scene escenaVentanaInicio = new Scene(ventanaInicio, 640, 480);
+        TableroDeJuego tableroDeJuego = new TableroDeJuego(stage);
+        Scene escenaTablero = new Scene(tableroDeJuego, 712, 680);
+        escenaTablero.getStylesheets().add("Vista/Estilos/estilos.css");
+
+        VentanaNombreJugadores ventanaNombreJugadores = new VentanaNombreJugadores(stage, escenaTablero, informacion, tableroDeJuego);
+        Scene escenaNombreJugadores = new Scene(ventanaNombreJugadores, 712, 680);
+        escenaNombreJugadores.getStylesheets().add("Vista/Estilos/estilos.css");
+
+        VentanaInicio ventanaInicio = new VentanaInicio(stage, escenaNombreJugadores);
+        Scene escenaVentanaInicio = new Scene(ventanaInicio, 712, 680);
         escenaVentanaInicio.getStylesheets().add("Vista/Estilos/estilos.css");
 
         stage.addEventHandler(KeyEvent.KEY_RELEASED, new BotonEscapeHandler(stage));
