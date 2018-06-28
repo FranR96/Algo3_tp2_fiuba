f1bf072cc811d5af016abfb2eb636b748742c592
MauRodriguez <drummer.4747@gmail.com>
2018-06-27 22:12:23 -0300

    LifePoints

--- Al-Go-Oh!/src/Vista/TableroDeJuego.java
+++ Al-Go-Oh!/src/Vista/TableroDeJuego.java
@@ -3,9 +3,15 @@ package Vista;
 import alGoOh.Jugador;
 import alGoOh.Tablero;
 import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+
+import static javafx.scene.paint.Color.*;
+
 
 public class TableroDeJuego extends HBox {
     private Stage stage;
@@ -19,10 +25,25 @@ public class TableroDeJuego extends HBox {
         this.jugador1 = jugador1;
         this.jugador2 = jugador2;
 
+        Label vida1 = new Label();
+        vida1.setText("LIFEPOINTS: " + jugador1.getPtsVida());
+        vida1.setTextFill(DARKRED);
+        vida1.setFont(Font.font ("Verdana",FontWeight.BOLD, 26));
+
+        Label vida2 = new Label();
+        vida2.setText("LIFEPOINTS: " + jugador2.getPtsVida());
+        vida2.setTextFill(DARKRED);
+        vida2.setFont(Font.font ("Verdana",FontWeight.BOLD, 26));
+
         GridPane cuadriculaTablero = new GridPane();
         cuadriculaTablero.getStyleClass().add("grilla-tablero");
         cuadriculaTablero.setGridLinesVisible(true);
         this.setGridConstraints(cuadriculaTablero);
+        GridPane.setColumnSpan(vida1, 12);
+        GridPane.setColumnSpan(vida2, 12);
+
+        cuadriculaTablero.add(vida1, 0,8);
+        cuadriculaTablero.add(vida2, 0,0);
 
         VBox cartasDeMano = new VBox();
         ScrollPane panelScrolleableDeCartas = new ScrollPane();
@@ -71,5 +92,6 @@ public class TableroDeJuego extends HBox {
         grid.getRowConstraints().add(new RowConstraints(107));
         grid.getRowConstraints().add(new RowConstraints(26));
         grid.getRowConstraints().add(new RowConstraints(108));
+        grid.getRowConstraints().add(new RowConstraints(45));
     }
 }
